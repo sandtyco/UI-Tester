e235bbe6c2d39321183583a0a22a86a3bb210010
Educollabs <tyco.sandtyco@yahoo.co.id>
2023-01-16 11:59:35 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html xmlns='http://www.w3.org/1999/xhtml' xmlns:b='http://www.google.com/2005/gml/b' xmlns:data='http://www.google.com/2005/gml/data' xmlns:expr='http://www.google.com/2005/gml/expr'>
+	<head>
+		<meta content='text/html; charset=UTF-8' http-equiv='Content-Type'/>
+		<meta charset='utf-8'/>
+		<meta content='width=device-width' name='viewport'/>
+		<meta content='Sandtyco' name='author'/>
+		<meta content='Educollabs, Education, Technology, UI, Responsive Design, Website Design' name='keywords'/>
+		<meta content='Show off how responsive design looks across different browsers easily with this 4 in 1 viewport tool. See how your site looks on desktop, laptop, ipad and iphone.' name='description'/>
+		<title>Educollabs - UI Testing</title>
+		<link href='http://info.#/responsive/responsive-web-design-tools.png' rel='image_src'/>
+		<link href='./ico/favicon.png' rel='icon' type='image/x-icon'/>
+		<link href='http://responsive.#/' rel='canonical'/>
+		<link href='http://responsive.#/feeds/posts/default' rel='alternate' title='Responsive # - Atom' type='application/atom+xml'/>
+		<link href='http://responsive.#/feeds/posts/default?alt=rss' rel='alternate' title='Responsive # - RSS' type='application/rss+xml'/>
+		<link href='http://www.blogger.com/feeds/8584774859049853945/posts/default' rel='alternate' title='Responsive # - Atom' type='application/atom+xml'/>
+		<link type='text/css' rel='stylesheet' href='./css/bootstrap.css'/>
+		<link type='text/css' rel='stylesheet' href='./css/bootstrap-min.css'/>
+		<style type='text/css'>
+			body{background-color:#fff;background-image:url(./img/background.jpg);background-size:100%;background-size:cover;font-family:Arial,Segoe UI,Trebuchet,Sans-Serif;color:#000;font-size:11px;margin:0;padding:0}a,#header-wrapper,#navinti li{-webkit-transition:all .4s ease-in-out;-moz-transition:all .4s ease-in-out;-o-transition:all .4s ease-in-out;-ms-transition:all .4s ease-in-out;transition:all .4s ease-in-out}a:hover{color:#999;text-decoration:underline}a:link,a:visited{color:#000;text-decoration:none}#outer-wrapper{margin:0 auto;width:100%;height:100%;padding:0;text-align:left;overflow:hidden}#nav{width: 100%;background-color: #fff;display: inline-block;border-top: 3px solid #888;border-bottom:3px solid #2F3A67;z-index: 9;}#navinti{margin:0 25px;padding:0;float:right;}#navinti li{list-style:none;zoom:1;float:left;display: inline-block;position:relative;margin:0;padding:0}#navinti li a{display:block;line-height:4.2em;vertical-align:baseline;font-size:1.5em;font-weight:400;text-transform:uppercase;text-decoration:none;color:#2F3A67;padding:0 10px}#navinti li:hover > a{color:#fff;} #navinti li:hover {background-color:#888}#navinti ul a:hover{background-color:#777}#navinti ul {position: absolute;text-align: left;width: 15em;white-space:nowrap;background-color: #F8F8F8;z-index: 99;padding: 0;list-style: none;display: none;}#navinti li:hover ul.hidden{display:block}#navinti ul a{line-height:35px;color:#2F3A67;font-size:1.2em;font-weight:normal;}#navinti ul li{border-bottom: 1px solid #ccc;border-right: 1px solid #ccc;border-left: 1px solid #ccc;float: none;display: block;}#navinti li.dgnsub > a {padding: 0 15px;}.bungkusin{width:1150px;margin:0 auto;display:block}.bungkusin-wrapper{margin:0 auto;height:100%;width:100%;float:left}.status-msg-wrap{width:100%}.status-msg-body{font-size:1.6em;text-transform:uppercase;text-align:center;margin:0;line-height:2em;color:#fff;background-color:#888}#header-wrapper{z-index:100;margin:0;float:left;position:absolute}#header-wrapper:hover{background:#888}#header{text-align:center;color:#fff;margin:0}#header h1,#header p{margin:0;padding:0 5px;line-height:70px;font-size:40px;font-weight:700}#header a{color:#fff;text-decoration:none}#header a:hover{color:#333;cursor:pointer}#header .description{display:none}#header img{height:auto;width:100%;display:block;margin-left:auto;margin-right:auto}#homepage-wrapper{background:#2F3A67;width:100%;margin:-3px 0 10px;display:block;text-align:center}#homepage-wrapper .icon{padding:30px 0 0}#homepage-wrapper .icon h1{font-weight:400;font-size:2.3em;color:#fff;text-transform:uppercase;line-height:1.5em;padding:0;margin:0}#homepage-wrapper .pencarian{margin:0 auto 10px;text-align:center;color:#fff;padding:0 1em 1em}#homepage-wrapper .tuliscari{padding:3px 10px;border:0;font-size:1.3em;line-height:1.5em;width:240px;margin:0 0 10px;color:#888}#homepage-wrapper .tombol{background:#999;text-transform:uppercase;font-size:1.3em;margin:0 0 0 10px;padding:5px 21px;border:1px solid #999;color:#fff;cursor:pointer;display:inline-block}#homepage-wrapper .formcari{display:inline-block;margin:0 auto;padding:10px 0}#bottom-wrapper{border-top:1px solid #888;width:100%;margin-top:40px;display:inline-block;float:left;background:#2F3A67}.credit{padding:10px;text-align:center}.credit p{font-size:1.4em;line-height:1.5em;margin:0;color:#fff;text-decoration:none}.credit a:link,.credit a:visited{color:#fff}.credit a:hover{color:#eee}fieldset{border:none}label{float:left;text-indent:-99999px}.wrapper{width:90%;max-width:1200px;margin:0 auto;overflow-y:hidden}.display{position:relative;left:0;height:600px}.display div{background-repeat:no-repeat;background-size:100%;overflow:hidden}iframe{transform:scale(0.219);-webkit-transform:scale(0.219);-o-transform:scale(0.219);-ms-transform:scale(0.219);-moz-transform:scale(0.219);transform-origin:top left;-webkit-transform-origin:top left;-o-transform-origin:top left;-ms-transform-origin:top left;-moz-transform-origin:top left;margin:0;padding:0;position:relative;background-color:#fff;border-color:#000}.mobile{background-image:url(./img/iphone.png);position:absolute;width:95px;height:196px;top:375px;left:300px;z-index:5}.mobile iframe{width:320px;height:480px;top:32px;left:11px;overflow-y:hidden}.tablet{background-image:url(./img/ipad.png);width:246px;height:400px;z-index:3;position:absolute;left:120px;top:230px}.tablet iframe{width:768px;height:1024px;top:35px;left:38px;overflow-y:hidden}.laptop{background-image:url(./img/laptop.png);width:477px;height:307px;top:264px;left:560px;position:absolute;z-index:2}.laptop iframe{width:1280px;height:802px;top:26px;left:60px;transform:scale(0.277);-webkit-transform:scale(0.277);-o-transform:scale(0.277);-ms-transform:scale(0.277);-moz-transform:scale(0.277);transform-origin:top left;-webkit-transform-origin:top left;-o-transform-origin:top left;-ms-transform-origin:top left;-moz-transform-origin:top left}.desktop{position:absolute;width:566px;height:538px;background-image:url(./img/desktop.png);top:0;left:220px;z-index:1}.desktop iframe{left:28px;top:38px;width:1600px;height:992px;transform:scale(0.3181);-webkit-transform:scale(0.3181);-o-transform:scale(0.3181);-ms-transform:scale(0.3181);-moz-transform:scale(0.3181);transform-origin:top left;-webkit-transform-origin:top left;-o-transform-origin:top left;-ms-transform-origin:top left;-moz-transform-origin:top left}@media (max-width: 1160px){.display{width:95%;height:550px;transform:scale(0.81);-webkit-transform:scale(0.81);-o-transform:scale(0.81);-ms-transform:scale(0.81);-moz-transform:scale(0.81)}.desktop{left:180px}.laptop{left:520px}.tablet{left:80px}.mobile{left:260px}}@media (max-width: 1070px){.display{left:-50px}}@media (max-width: 1000px){.display{height:500px;transform:scale(0.71);-webkit-transform:scale(0.71);-o-transform:scale(0.71);-ms-transform:scale(0.71);-moz-transform:scale(0.71);top:-40px}.desktop{left:140px}.laptop{left:480px}.tablet{left:40px}.mobile{left:220px}}@media (max-width: 850px){.display{height:500px;transform:scale(0.65);-webkit-transform:scale(0.65);-o-transform:scale(0.65);-ms-transform:scale(0.65);-moz-transform:scale(0.65)}.desktop{left:100px}.laptop{left:440px}.tablet{left:0}.mobile{left:180px}}@media (max-width: 768px){.display{height:450px;transform:scale(0.55);-webkit-transform:scale(0.55);-o-transform:scale(0.55);-ms-transform:scale(0.55);-moz-transform:scale(0.55)}}@media (max-width: 670px){.display{height:400px;left:-70px;transform:scale(0.45);-webkit-transform:scale(0.45);-o-transform:scale(0.45);-ms-transform:scale(0.45);-moz-transform:scale(0.45)}}@media (max-width: 560px){.display{height:270px;top:-65px;transform:scale(0.37);-webkit-transform:scale(0.37);-o-transform:scale(0.37);-ms-transform:scale(0.37);-moz-transform:scale(0.37)}}@media (max-width: 440px){.display{left:-17px;top:-65px;width:70px;transform:scale(0.35);-webkit-transform:scale(0.35);-o-transform:scale(0.35);-ms-transform:scale(0.35);-moz-transform:scale(0.35)}}@media (max-width: 380px){.display{height:235px;left:-17px;top:-65px;width:70px;transform:scale(0.27);-webkit-transform:scale(0.27);-o-transform:scale(0.27);-ms-transform:scale(0.27);-moz-transform:scale(0.27)}.desktop{left:100px}.laptop{left:515px}.tablet{left:0}.mobile{left:180px}}
+		</style>
+			<!--[if IE 8]> <style> .desktop iframe{-ms-filter:"progid:DXImageTransform.Microsoft.Matrix(M11=0.3181,M12=0,M21=0,M22=0.3181,SizingMethod='auto expand')"}.laptop iframe{-ms-filter:"progid:DXImageTransform.Microsoft.Matrix(M11=0.277,M12=0,M21=0,M22=0.277,SizingMethod='auto expand')"}.tablet iframe{-ms-filter:"progid:DXImageTransform.Microsoft.Matrix(M11=0.234,M12=0,M21=0,M22=0.234,SizingMethod='auto expand')"}.mobile iframe{-ms-filter:"progid:DXImageTransform.Microsoft.Matrix(M11=0.219,M12=0,M21=0,M22=0.219,SizingMethod='auto expand')"} </style> <![endif]-->
+			<!--[if lte IE 7]> <style> .desktop iframe{filter:progid:DXImageTransform.Microsoft.Matrix(M11=0.3181,M12=0,M21=0,M22=0.3181,SizingMethod='auto expand')}.laptop iframe{filter:progid:DXImageTransform.Microsoft.Matrix(M11=0.277,M12=0,M21=0,M22=0.277,SizingMethod='auto expand')}.tablet iframe{filter:progid:DXImageTransform.Microsoft.Matrix(M11=0.234,M12=0,M21=0,M22=0.234,SizingMethod='auto expand')}.mobile iframe{filter:progid:DXImageTransform.Microsoft.Matrix(M11=0.219,M12=0,M21=0,M22=0.219,SizingMethod='auto expand')} </style> <![endif]-->
+		<script type="text/javascript">
+			if (window.jstiming) window.jstiming.load.tick('headEnd');
+		</script>
+	</head>
+	
+	<body>
+	<div>
+		<div id='homepage-wrapper'>
+			<div class='icon'>
+				<h1 style="font-family: cambria;">Website-Based Application User Interface Test Tool</h1>
+				<h4 style="font-family: cambria; color: white;">Please enter the website address URL in the form below...</h4>
+			</div>
+			<div class='pencarian'>
+				<form class='formcari' id='rwdform' name='rwdform'>
+					<input class='input-large search-query' id='url' name='url' placeholder='http://' type='url' value='http://'/>
+					<a class='btn btn-info' href='#' onclick='_gaq.push([&#39;_trackEvent&#39;, &#39;RWD&#39;, &#39;URL&#39;, document.rwdform.url.value]);'>Run Check</a>
+				</form>
+			</div>
+		</div>
+		
+		<div class='wrapper'>
+			<section class='display'>
+				<div class='mobile ui-draggable'><div class='trim'><iframe id='mobile' src=''></iframe></div></div>
+				<div class='tablet ui-draggable'><div class='trim'><iframe id='tablet' src=''></iframe></div></div>
+				<div class='laptop ui-draggable'><div class='trim'><iframe id='laptop' src=''></iframe></div></div>
+				<div class='desktop ui-draggable'><div class='trim'><iframe id='desktop' src=''></iframe></div></div>
+			</section>
+		</div>
+		<div id='bottom-wrapper'>
+			<div id='bottomdown'>
+				<div class='footer section' id='footer2'>
+					<div class='widget HTML' id='HTML01'>
+						<div class='widget-content credit'>
+							<p>Copyright <strong>&copy;</strong> 2022 GNU/GPL | UI Tester. All rights reserved.&nbsp;&nbsp; | &nbsp;&nbsp;Development By : <a href="http://educollabs.org">Educollabs</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<script src='./js/jquery-latest.js'></script>
+	<script src='./js/jquery-ui.js'></script>
+	<script type='text/javascript'>
+		//<![CDATA[
+			$(document).ready(function() {$(".tombol").click(function(event){$("iframe").attr('src', $( '#url' ).val());});});
+			$(document).ready(function() {var a = 3;$('.desktop,.laptop,.tablet,.mobile').draggable({start: function(event, ui) { $(this).css("z-index", a++); }});$('.display div').click(function() {$(this).addClass('top').removeClass('bottom');$(this).siblings().removeClass('top').addClass('bottom');$(this).css("z-index", a++);});});
+			function getUrlVars() {var vars = {};var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {vars[key] = value;});return vars;}var first = getUrlVars()["url"];var first = decodeURIComponent(first);var first = first.replace(/\#$/, '');if(first === "undefined") {}else {$("iframe").attr('src',(first));$('#url').attr('value',(first));}
+		//]]>
+	</script>
+
+	</body>
+</html>
